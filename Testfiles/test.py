60f58b75ea664b7dd9a7c2c4a5e79cc1a3fa119c
Philemon Huising <phuising@yahoo.co.uk>
2021-11-16 18:47:17 +0100

    minor stuff

--- Testfiles/test.py
+++
@@ -1,5 +0,0 @@
-# Import functions from peerGrading_multi
-from peerGrading_multi import *
-
-# We should be able to do all the testing in these kinds of files. That keeps the original encodings and such clean.
-
